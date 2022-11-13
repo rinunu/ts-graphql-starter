252f4d077e854c72bb8d43af83efce8533ffd5f8
rinunu <rintaro.t@gmail.com>
2022-11-13 17:54:23 +0900

    説明を追記

--- jest.preset.js
+++ jest.preset.js
@@ -3,5 +3,6 @@ const nxPreset = require('@nrwl/jest/preset').default;
 /** @type {import('ts-jest').InitialOptionsTsJest} */
 module.exports = {
   ...nxPreset,
+  // docs/testing.md 参照
   maxWorkers: 1,
 };
